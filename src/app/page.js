56df50f79bbc6504e31762c7a82c86cfef9c039e
ExpErnand <expeditoernande2605@gmail.com>
2024-03-20 15:32:24 -0300

    New header

--- src/app/page.js
+++ src/app/page.js
@@ -3,29 +3,41 @@ import styles from "./page.module.css";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 
 export default function Home() {
   return (
     <section>
       <header className="cabecalho">
-        <ul className="listaicones">
-          <li className="icones">
-            <a href="https://www.facebook.com/monomitofilmes/" target="_blank"> 
-            <FaFacebookSquare size="4em" color="white"/> 
-            </a>
-          </li>
-          <li className="icones">
-            <a href="https://www.instagram.com/monomitofilmes/" target="_blank">
-              <FaInstagram size="4em" color="white" />
-            </a>
-          </li>
-          <li className="icones">
-            <a href="https://www.youtube.com/channel/UCFacycfmcN-9HwOue4qE07Q" target="_blank"> 
-            <FaYoutube size="4em" color="white"/> 
-            </a>
-          </li>
-        </ul>
+        <div className="monomito-title">
+          <a href="#"><h1 className="header-title">Monomito Filmes</h1></a>
+          </div>
+            <ul className="headerlist">
+              <a href="#"><li className="headeritemlist">Início</li></a>
+              <a href="#"><li className="headeritemlist">Projeto</li></a>
+              <a href="#"><li className="headeritemlist">Sobre</li></a>
+              <a href="#"><li className="headeritemlist">Contatos</li></a>
+            </ul> 
+            <ul className="listaicones">
+              <li className="icones">
+                <a href="https://www.facebook.com/monomitofilmes/" target="_blank"> 
+                <FaFacebookSquare size="1.4em" color="white"/> 
+                </a>
+              </li>
+              <li className="icones">
+                <a href="https://www.instagram.com/monomitofilmes/" target="_blank">
+                  <FaInstagram size="1.4em" color="white" />
+                </a>
+              </li>
+              <li className="icones">
+                <a href="https://www.youtube.com/channel/UCFacycfmcN-9HwOue4qE07Q" target="_blank"> 
+                <FaYoutube size="1.4em" color="white"/> 
+                </a>
+              </li>
+            </ul>
       </header>
+
+
 
       <main className={styles.main}>
         <div className={styles.center}>
