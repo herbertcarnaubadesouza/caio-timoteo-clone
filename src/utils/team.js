315f2cb41606922f6cc8b1fad33f15676e2f5a43
Caio Timoteo <caiocesar.wk@gmail.com>
2024-10-07 16:18:08 -0300

    remove Felipe

--- src/utils/team.js
+++ src/utils/team.js
@@ -1,42 +1,42 @@
 export const team = [
-    {
-        id: 1,
-        photo: "/foto-lincoln.png",
-        name: "Lincoln Pires",
-        function: "Produtor Criativo",
-        descriptionOne: "Lincoln Pires, 35 anos, Diretor e fotógrafo, co-fundou o Coletivo Monomito Filmes, que realizava produções audiovisuais, com foco em contar novas histórias, mais diversas, com um olhar voltado a lugares que a muito não tinham atenção.",
-        descriptionTwo: "Começou a documentar batalhas de poesia e criar materiais para artistas e músicos gratuitamente, com esse projeto nasceu o Tarja Preta, biblioteca de vídeo poesias, contendo mais de 250 poetas marginais das mais diferentes periferias de São Paulo e do Brasil, hoje ele dirige a Monomito Filmes, uma produtora audiovisual de impacto social."
-    },
-    {
-        id: 2,
-        photo: "/foto-larissa.png",
-        name: "Larissa",
-        function: "CFO - Produção executiva",
-        descriptionOne: "Larissa Souza, 28 anos, Produtora Cultural, em 2017 co-fundou o Coletivo Monomito Filmes, que realizava produções audiovisuais, com foco em contar novas histórias, mais diversas, com um olhar voltado a lugares que a muito não tinham atenção.",
-        descriptionTwo: "Começou a documentar batalhas de poesia e criar materiais para artistas e músicos gratuitamente, com esse projeto nasceu o Tarja Preta, biblioteca de vídeo poesias, contendo mais de 250 poetas marginais das mais diferentes periferias de São Paulo e do Brasil."
-    },
-    {
-        id: 3,
-        photo: "/foto-brenda-barbosa.png",
-        name: "Brenda Barbosa",
-        function: "CEO - Criativo",
-        descriptionOne: "Brenda Barbosa, 25 anos, Diretora e videomaker, graduada em Comunicação Social. Sua paixão pela fotografia e cinema começou aos 14 anos, quando iniciou sua jornada na fotografia e edição.",
-        descriptionTwo: "Em 2020, fundou sua produtora de videoclipes, onde nasceu o projeto “A Voz da Quebrada”, onde foram produzidos 50 videoclipes para artistas independentes da Zona Leste e impactou mais de 400 mil pessoas. Atualmente sócio e diretor criativo na Monomito Filmes, já realizou projetos com empresas como Amazon, Google, TikTok, Samsung, Accor, e Netflix."
-    },
-    {
-        id: 4,
-        photo: "/foto-felipe-bee.png",
-        name: "Felipe Bee",
-        function: "Produtor Criativo",
-        descriptionOne: "Felipe Bee, 28 anos, Criativo e Videomaker, graduado em Publicidade e Propaganda pela UNICSUL, apaixonado por contar histórias através do audiovisual, começou a editar seus primeiros projetos autorais com apenas 13 anos, em 2020 fundou uma, produtora de videoclipes.",
-        descriptionTwo: "Nisso nasceu o projeto “A Voz da Quebrada”, onde foram produzidos 50 videoclipes para artistas independentes da Zona Leste. Atualmente sócio e diretor criativo na Monomito Filmes, já realizou projetos com empresas como Amazon, Google, TikTok, Accor e Netflix."
-    },
-    {
-        id: 5,
-        photo: "/foto-lincoln-paiva.png",
-        name: "Linkyn Paiva",
-        function: "Produtor Criativo",
-        descriptionOne: "Linkyn Paiva, Social mídia e videomaker de 25 anos, formado em Sound Design pela EBAC - Escola Britânica de Artes Criativas e Tecnologia, idealizador do projeto “A Voz da Quebrada“ alcançando diretamente 400 mil pessoas e amplificando o trabalho de jovens MCs em territórios de periferia e favela.",
-        descriptionTwo: "Atualmente, Linkin é sócio criativo da Monomito Filmes, onde já participou de projetos para gigantes como Amazon, Google, TikTok, Accor e Netflix, levando o talento e a autenticidade da periferia para o cenário global."
-    },
-]
+  {
+    id: 1,
+    photo: '/foto-lincoln.png',
+    name: 'Lincoln Pires',
+    function: 'Produtor Criativo',
+    descriptionOne:
+      'Lincoln Pires, 35 anos, Diretor e fotógrafo, co-fundou o Coletivo Monomito Filmes, que realizava produções audiovisuais, com foco em contar novas histórias, mais diversas, com um olhar voltado a lugares que a muito não tinham atenção.',
+    descriptionTwo:
+      'Começou a documentar batalhas de poesia e criar materiais para artistas e músicos gratuitamente, com esse projeto nasceu o Tarja Preta, biblioteca de vídeo poesias, contendo mais de 250 poetas marginais das mais diferentes periferias de São Paulo e do Brasil, hoje ele dirige a Monomito Filmes, uma produtora audiovisual de impacto social.',
+  },
+  {
+    id: 2,
+    photo: '/foto-larissa.png',
+    name: 'Larissa',
+    function: 'CFO - Produção executiva',
+    descriptionOne:
+      'Larissa Souza, 28 anos, Produtora Cultural, em 2017 co-fundou o Coletivo Monomito Filmes, que realizava produções audiovisuais, com foco em contar novas histórias, mais diversas, com um olhar voltado a lugares que a muito não tinham atenção.',
+    descriptionTwo:
+      'Começou a documentar batalhas de poesia e criar materiais para artistas e músicos gratuitamente, com esse projeto nasceu o Tarja Preta, biblioteca de vídeo poesias, contendo mais de 250 poetas marginais das mais diferentes periferias de São Paulo e do Brasil.',
+  },
+  {
+    id: 3,
+    photo: '/foto-brenda-barbosa.png',
+    name: 'Brenda Barbosa',
+    function: 'CEO - Criativo',
+    descriptionOne:
+      'Brenda Barbosa, 25 anos, Diretora e videomaker, graduada em Comunicação Social. Sua paixão pela fotografia e cinema começou aos 14 anos, quando iniciou sua jornada na fotografia e edição.',
+    descriptionTwo:
+      'Em 2020, fundou sua produtora de videoclipes, onde nasceu o projeto “A Voz da Quebrada”, onde foram produzidos 50 videoclipes para artistas independentes da Zona Leste e impactou mais de 400 mil pessoas. Atualmente sócio e diretor criativo na Monomito Filmes, já realizou projetos com empresas como Amazon, Google, TikTok, Samsung, Accor, e Netflix.',
+  },
+  {
+    id: 4,
+    photo: '/foto-lincoln-paiva.png',
+    name: 'Linkyn Paiva',
+    function: 'Produtor Criativo',
+    descriptionOne:
+      'Linkyn Paiva, Social mídia e videomaker de 25 anos, formado em Sound Design pela EBAC - Escola Britânica de Artes Criativas e Tecnologia, idealizador do projeto “A Voz da Quebrada“ alcançando diretamente 400 mil pessoas e amplificando o trabalho de jovens MCs em territórios de periferia e favela.',
+    descriptionTwo:
+      'Atualmente, Linkin é sócio criativo da Monomito Filmes, onde já participou de projetos para gigantes como Amazon, Google, TikTok, Accor e Netflix, levando o talento e a autenticidade da periferia para o cenário global.',
+  },
+];
